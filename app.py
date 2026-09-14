55657c221c6e8e3314d63e8aa18a38689ae2d364
Surrey Data Science Team <123080142+SCCDataScience@users.noreply.github.com>
2026-09-14 16:15:21 +0100

    Update app.py
    zoom and centring

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ with tab_dashboard:
             if geo:
                 fig_map = px.choropleth_map(map_df_active, geojson=geo, locations="Area_Name", featureidkey="properties.LAD23NM",
                     color="Value", color_continuous_scale="viridis", map_style="open-street-map",
-                    zoom=8.5, center={"lat": 51.26, "lon": -0.4}, opacity=0.6)
+                    zoom=8.6, center={"lat": 51.27, "lon": -0.4}, opacity=0.6)
                 fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
                 st.plotly_chart(fig_map, use_container_width=True)
         with col2:
